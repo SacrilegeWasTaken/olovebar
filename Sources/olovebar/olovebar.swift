7827ee3e05474620e83ee9b35fd4467e3ca1bf46
SacrilegeWasTaken <superdjskater@mail.ru>
2025-10-13 03:22:55 +0700

    get active window

--- Sources/olovebar/olovebar.swift
+++ Sources/olovebar/olovebar.swift
@@ -1,20 +1,31 @@
 import Cocoa
 import MetalKit
+import CoreGraphics
 
-final class BarWindow: NSWindow {
-    override var canBecomeKey: Bool {
-        false
+
+final class BarWindow: NSWindow { 
+    override var canBecomeKey: Bool { 
+        false 
     }
-    override var canBecomeMain: Bool {
-        false
-    }
+    override var canBecomeMain: Bool { 
+        false 
+    } 
 }
+
 
 final class AppDelegate: NSObject, NSApplicationDelegate {
     var window: BarWindow!
     var metalView: MTKView!
 
     public func applicationDidFinishLaunching(_ notification: Notification) {
+        print("Application did finish launching")
+        setupWindow()
+        subscribeNotifications()
+    }
+    
+    @MainActor
+    private func setupWindow() {
+        print("Setting up window...")
         let barHeight: CGFloat = 35
         let barHorizontalCut: CGFloat = 10
         let barVerticalCut: CGFloat = 4
@@ -25,9 +36,6 @@ final class AppDelegate: NSObject, NSApplicationDelegate {
             width: screenFrame.width - barHorizontalCut * 2,
             height: barHeight - barVerticalCut 
         )
-
-
-        print("Screen frame: \(frame)")
         
         window = BarWindow(
             contentRect: frame,
@@ -35,7 +43,7 @@ final class AppDelegate: NSObject, NSApplicationDelegate {
             backing: .buffered,
             defer: false
         )
-
+        
         window.isOpaque = false
         window.backgroundColor = NSColor.clear
         window.level = NSWindow.Level(rawValue: NSWindow.Level.statusBar.rawValue - 1)
@@ -44,16 +52,14 @@ final class AppDelegate: NSObject, NSApplicationDelegate {
         window.hasShadow = false
         window.isReleasedWhenClosed = false
         window.orderBack(nil)
-
-        // Контейнер для Metal
+        
         let container = NSView(frame: frame)
         container.wantsLayer = true
         container.layer = CALayer()
         container.layer?.backgroundColor = NSColor.windowBackgroundColor.withAlphaComponent(0.4).cgColor
         container.layer?.cornerRadius = 16
         container.layer?.masksToBounds = true
-
-        // Metal внутри контейнера
+        
         metalView = MTKView(frame: container.bounds)
         metalView.autoresizingMask = [.width, .height]
         metalView.device = MTLCreateSystemDefaultDevice()
@@ -61,15 +67,43 @@ final class AppDelegate: NSObject, NSApplicationDelegate {
         metalView.framebufferOnly = false
         metalView.isPaused = false
         metalView.enableSetNeedsDisplay = true
-
-        // Metal слой подчиняется маске
         metalView.wantsLayer = true
         metalView.layer?.cornerRadius = 16
         metalView.layer?.masksToBounds = true
-
+        
         container.addSubview(metalView)
         window.contentView = container
-
+        
         window.makeKeyAndOrderFront(nil)
+    }
+
+    private func subscribeNotifications() {
+        // Subscribe to notifications by system events
+        let ncs = NSWorkspace.shared.notificationCenter
+
+        // Subscribe to notifications by this app events
+        // `let ncd = NotificationCenter.default`
+
+        ncs.addObserver(
+            self, 
+            selector: #selector(updateState(_:)), 
+            name: NSWorkspace.activeSpaceDidChangeNotification, 
+            object: nil
+        )
+        ncs.addObserver(
+            self,
+            selector: #selector(updateState(_:)),
+            name: NSWorkspace.didActivateApplicationNotification,
+            object: nil
+        )
+    }
+
+    @objc private func updateState(_ notification: Notification) {
+        print("Updating state...")
+        guard let userInfo = notification.userInfo,
+              let runningApp = userInfo[NSWorkspace.applicationUserInfoKey] as? NSRunningApplication,
+              let appName = runningApp.localizedName else { return }
+
+        print("Active application: \(appName)")
     }
 }
