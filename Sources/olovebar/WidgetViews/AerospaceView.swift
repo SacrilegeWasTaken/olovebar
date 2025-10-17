0354b2c187d42e1cb3098831d07de0b64b1ecbb7
SacrilegeWasTaken <superdjskater@mail.ru>
2025-10-17 13:52:50 +0700

    aerospace back

--- Sources/olovebar/WidgetViews/AerospaceView.swift
+++ Sources/olovebar/WidgetViews/AerospaceView.swift
@@ -15,32 +15,18 @@ struct AerospaceWidgetView: View {
             ForEach(model.workspaces, id: \.self) { id in
                 Button(action: { withAnimation { model.focus(id) } }) {
                     let _ = debug("Updating text UI")
-                    ZStack {
-                        Text(id)
-                            .font(.system(size: 12, weight: .medium))
-                            .frame(width: config.aerospaceWidth, height: config.widgetHeight)
-                            .foregroundColor(.white)
-                            .background(.clear)
-                        if self.model.focused == id {
-                            LiquidGlassBackground(
-                                variant: .v11,
-                                cornerRadius: config.widgetCornerRadius
-                            ) {
-                                // Color.clear
-                                // // НЕ Color.clear - нужен реальный контент
-                                Rectangle()
-                                    .fill(.clear)
-                                    .frame(width: config.aerospaceWidth * 2, height: config.widgetHeight)
-                            }
-                            .frame(width: config.aerospaceWidth * 2, height: config.widgetHeight)
-                        }
-                    }
+                    Text(id)
+                        .font(.system(size: 12, weight: .medium))
+                        .frame(width: config.aerospaceWidth, height: config.widgetHeight)
+                        .foregroundColor(id == self.model.focused ? .purple : .white)
+                        .background(.clear)
+                    
                 }
                 .buttonStyle(.plain)
                 .clipShape(RoundedRectangle(cornerRadius: config.widgetCornerRadius, style: .continuous))
                 .background(.clear)
                 .frame(width: config.aerospaceWidth, height: config.widgetHeight)
-                .animation(.interpolatingSpring(duration: 0.2), value: model.focused)
+                .animation(.interpolatingSpring(duration: 0.1), value: model.focused)
             }
         }
         .background(.clear)
