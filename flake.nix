552f0e8a6538bcf5359c2d8abdd861d1472f7ad8
Leonid <superdjskater@mail.ru>
2026-03-11 23:27:49 +0700

    install via nixxx

--- flake.nix
+++ flake.nix
@@ -1,19 +1,61 @@
-{ stdenv, fetchurl }:
+{
+  description = "OLoveBar — несколько версий по феншую";
 
-stdenv.mkDerivation {
-  pname = "olovebar";
-  version =
-    lib.strings.trim (builtins.readFile ./VERSION);
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
-  src = fetchurl {
-    url = "https://codeberg.org/sacrilegewastaken/olovebar/releases/download/${version}/OLoveBar.dmg";
-    sha256 = "sha256-xZW+zdDMMNK3ylvYmdWqX3RQP6mrS+2E9BsGsOhe1a8=";
+  outputs = { self, nixpkgs }: let
+    system = "aarch64-darwin";
+    pkgs   = import nixpkgs { inherit system; };
+    lib    = pkgs.lib;
+
+    mkOlovebar = { version, sha256 }: pkgs.stdenv.mkDerivation {
+      pname   = "olovebar";
+      inherit version;
+
+      src = pkgs.fetchurl {
+        url    = "https://codeberg.org/sacrilegewastaken/olovebar/releases/download/${version}/OLoveBar.dmg";
+        inherit sha256;
+      };
+
+      installPhase = ''
+        mkdir -p $out/Applications
+        hdiutil attach "$src" -mountpoint /Volumes/OLoveBar -nobrowse -quiet
+        cp -R "/Volumes/OLoveBar/OLoveBar.app" "$out/Applications/OLoveBar.app"
+        hdiutil detach "/Volumes/OLoveBar" -quiet
+      '';
+    };
+
+    latestVersion = lib.strings.trim (builtins.readFile ./VERSION);
+    latestHash    = "sha256-3p0rtwAhxetfSLX48WjUypxNTg8yU70w+ETDIm/vcUA=";
+  in {
+    packages.${system} = {
+      olovebar = mkOlovebar {
+        version = latestVersion;
+        sha256  = latestHash;
+      };
+
+      olovebar_1_2_0 = mkOlovebar {
+        version = "1.2.0";
+        sha256  = "sha256-vypgvCrjMDuMRcGtzEzNiryHkmYHmYmJ/FNGkJ2ayP4=";
+      };
+
+      olovebar_1_3_0 = mkOlovebar {
+        version = "1.3.0";
+        sha256  = "sha256-kjKN23/dBQblEVGo+ySwx+WUmqetGcVDR87t48Wmo6c=";
+      };
+
+      olovebar_1_3_1 = mkOlovebar {
+        version = "1.3.1";
+        sha256  = "sha256-xZW+zdDMMNK3ylvYmdWqX3RQP6mrS+2E9BsGsOhe1a8=";
+      };
+
+      olovebar_1_3_2 = mkOlovebar {
+        version = "1.3.2";
+        sha256  = "sha256-3p0rtwAhxetfSLX48WjUypxNTg8yU70w+ETDIm/vcUA=";
+      };
+    };
+
+    # чтобы nix сам понимал, кто тут главный
+    packages.${system}.default = self.packages.${system}.olovebar;
   };
-
-  installPhase = ''
-    mkdir -p $out/Applications
-    hdiutil attach "$src" -mountpoint /Volumes/OLoveBar -nobrowse -quiet
-    cp -R "/Volumes/OLoveBar/OLoveBar.app" "$out/Applications/OLoveBar.app"
-    hdiutil detach "/Volumes/OLoveBar" -quiet
-  '';
 }
